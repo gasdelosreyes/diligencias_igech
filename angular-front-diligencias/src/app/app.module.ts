923c99370902c9e3fb372ec84dde23a27987aabc
Gaston De los Reyes <mu_gas@hotmail.com>
2020-06-08 14:38:19 -0300

    Angular material añadido

--- angular-front-diligencias/src/app/app.module.ts
+++ angular-front-diligencias/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/app-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Own Components
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
