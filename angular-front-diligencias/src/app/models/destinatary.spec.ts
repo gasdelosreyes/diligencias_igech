db00af9c9d9921954687bfa87e11a9e3734ccbda
Gaston De los Reyes <mu_gas@hotmail.com>
2020-06-29 12:30:57 -0300

    Borrar por que hice todo mal

--- angular-front-diligencias/src/app/models/destinatary.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Destinatary } from './destinatary';
-
-describe('Destinatary', () => {
-  it('should create an instance', () => {
-    expect(new Destinatary()).toBeTruthy();
-  });
-});
--- angular-front-diligencias/src/app/models/destinatary.ts
+++
@@ -1,7 +0,0 @@
-export class Destinatary {
-  id: String;
-  name: String;
-  address: String;
-  contact: String;
-  cost: Number;
-}
